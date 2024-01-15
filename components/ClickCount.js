83d90f5a10c1d4cd70b8d476537571754821921d
Peter Tasker <ptasker@github.com>
2024-01-15 13:37:27 -0500

    Fix #18: Add a reset button to the main page

--- components/ClickCount.js
+++ components/ClickCount.js
@@ -1,11 +1,17 @@
-import { useCallback, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import Button from './Button'
 
-export default function ClickCount() {
+export default function ClickCount({ reset }) { // Pa8b9
   const [count, setCount] = useState(0)
   const increment = useCallback(() => {
     setCount((v) => v + 1)
   }, [setCount])
+
+  useEffect(() => { // Pa8b9
+    if (reset > 0) {
+      setCount(0)
+    }
+  }, [reset, setCount])
 
   return <Button onClick={increment}>Clicks: {count}</Button>
 }
